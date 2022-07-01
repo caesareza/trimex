a348c8bc20ff9f5a2bf243733ab0e1bc5a1cf6e0
Reza Ola <dreas269@gmail.com>
2022-07-01 21:26:36 +0700

    add mobile menu
    membuat menu mobile

--- src/components/General/Header/index.tsx
+++ src/components/General/Header/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 import { menu } from '@/data/menu'
 import { IMenu } from '@/type/global'
+import { HiOutlineMenuAlt3 } from 'react-icons/hi'
+import { IoMdClose } from 'react-icons/io'
 
 export default function Header() {
+  const [isMenuShow, setIsMenuShow] = useState<boolean>(false)
+  const menuRef = useRef<any>(null)
+
+  const handleClickOutside = (event: any) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuShow(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside)
+    return () => document.removeEventListener('click', handleClickOutside)
+  }, [])
+
+  const MenuMobile = () => {
+    if (!isMenuShow) return <></>
+
+    return (
+      <section
+        id="menu-mobile"
+        className="bg-white fixed z-10 left-0 top-0 w-full h-full text-center"
+      >
+        <div className="container mx-auto relative">
+          <button
+            className="absolute right-5 top-7"
+            onClick={() => setIsMenuShow(!isMenuShow)}
+          >
+            <IoMdClose size={30} />
+          </button>
+          <h3 className="py-5">Menu</h3>
+          <ul className="">
+            {menu.map((value: IMenu) => (
+              <li key={value.keys}>
+                <Link href={value.href} as={value.as}>
+                  <a className="p-5 text-2xl p-5 border-b block">
+                    {value.label}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    )
+  }
+
   return (
     <header className="w-full bg-white">
       <div className="container mx-auto">
@@ -23,7 +71,7 @@ export default function Header() {
               </div>
             </a>
           </Link>
-          <ul className="ml-auto hidden lg:flex">
+          <ul className="ml-0 lg:ml-auto hidden lg:flex">
             {menu.map((value: IMenu) => (
               <li key={value.keys}>
                 <Link href={value.href} as={value.as}>
@@ -32,8 +80,18 @@ export default function Header() {
               </li>
             ))}
           </ul>
+
+          <button
+            className="ml-auto lg:ml-0"
+            onClick={() => setIsMenuShow(!isMenuShow)}
+            ref={menuRef}
+          >
+            <HiOutlineMenuAlt3 size={23} />
+          </button>
         </div>
       </div>
+
+      <MenuMobile />
     </header>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,10 +52,10 @@ const Home: NextPage = () => {
 
       <div className="bg-yellow-300 -mt-2">
         <div className="container mx-auto flex">
-          <div className="p-5 py-10 flex items-center justify-center w-full">
+          <div className="p-5 py-10 flex-col lg:flex-row flex items-center justify-center w-full">
             <h2 className="text-2xl font-bold">Enter your tracking code #</h2>
             <input
-              className="p-3 mx-3 w-1/2"
+              className="p-3 my-2 mx-3 w-1/2"
               placeholder="Nomor resi"
               onChange={handleInputTracking}
             />
